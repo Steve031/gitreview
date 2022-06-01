26d4602050ba87fcb6df204b0c467daf60e65659
stave031 <stevealba7382@gmail.com>
2022-05-31 21:54:00 -0400

    2nd commit

--- src/gitreview/Gitreview01.java
+++ src/gitreview/Gitreview01.java
@@ -3,8 +3,21 @@ package gitreview;
 public class Gitreview01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		System.out.println("today we learn github");
+		System.out.println("today we learn github");
+		System.out.println("today we learn github");
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
